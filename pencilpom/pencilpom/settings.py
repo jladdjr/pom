0591891dd16de07d083ed9bc2257fe3938854cc0
Jim Ladd <jladd@redhat.com>
2018-05-08 23:22:00 -0400

    Create models

--- pencilpom/pencilpom/settings.py
+++ pencilpom/pencilpom/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pom.apps.PomConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- pencilpom/pom/models.py
+++ pencilpom/pom/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Day(models.Model):
+    date = models.DateField()
+
+    def __str__(self):
+        return str(self.date)
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.project_name
+
+class Progress(models.Model):
+    day = models.ForeignKey(Day, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    poms = models.IntegerField(default=0)
+    goal = models.IntegerField(default=0)
+
+    def __str__(self):
+        return "{0.poms} / {0.goal}".format(self)
+
+    @property
+    def is_complete(self):
+        return poms >= goal
